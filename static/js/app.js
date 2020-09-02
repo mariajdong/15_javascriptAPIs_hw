b254b9cd398466b312612f5ae5d47bc522bf2995
Maria Dong <maria.j.dong@gmail.com>
2020-09-02 17:20:12 -0400

    added style.css, added gauge chart, misc. edits

--- static/js/app.js
+++ static/js/app.js
@@ -35,7 +35,7 @@ function build_charts (sample) {
             title: `Top 10 OTUs in Sample ${sample}`
         }
 
-        Plotly.newPlot ('bar', bar_data, bar_layout);
+        Plotly.newPlot ('bar', bar_data, bar_layout, {responsive: true});
 
         // create trace/data & layout for bubble chart
         var bubble_data = [{
@@ -53,14 +53,13 @@ function build_charts (sample) {
             title: `All Bacteria in Sample ${sample} & Corresponding Frequency`,
             xaxis: { title: 'OTU ID' },
             yaxis: { title: 'Frequency' }
-        }
-        Plotly.newPlot ('bubble', bubble_data, bubble_layout);
-
-        // create gauge chart
+        };
+        
+        Plotly.newPlot ('bubble', bubble_data, bubble_layout, {responsive: true});
     });
 }
 
-// fxn to build metadata panel
+// fxn to build metadata panel & gauge chart
 function build_metadata (sample) {
     
     // define url, fetch sample info from metadata
@@ -79,8 +78,40 @@ function build_metadata (sample) {
 
         Object.entries (sample_data).forEach (([key, value]) => {
             var row = metadata_box.append ('tr');
-            row.text (`${key}: ${value}`)
+            var cell = row.append ('td');
+            cell.text (`${key}: ${value}`)
+
+            // pull frequency of washing for gauge chart below
+            if (key == 'wfreq') {
+                wash_freq = value;
+            }; 
         });
+
+        // create gauge data
+        var gauge_data = [{
+            domain: { x: [0, 1], y: [0, 1] },
+            value: wash_freq,
+            gauge: { 
+                axis: { 
+                    range: [0, 10],
+                    dtick: 2
+                },
+                bar: { color: '#003333' },
+                steps: [
+                    { range: [0, 2], color: "#FFF9C4" },
+                    { range: [2, 4], color: "#F0F4C3" },
+                    { range: [4, 6], color: "#DCEDC8" },
+                    { range: [6, 8], color: "#C8E6C9" },
+                    { range: [8, 10], color: "#B2DFDB" }
+                ],
+            },
+            title: 'Washing Frequency',
+            type: "indicator",
+            mode: "gauge+number"
+        }];
+        
+        var gauge_layout = { width: 400, height: 450, margin: { l: -100, t: 0, b: 0 } };
+        Plotly.newPlot('gauge', gauge_data, gauge_layout, {responsive: true});
     });
 }
 
